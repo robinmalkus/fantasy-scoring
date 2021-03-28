5f7ba73ade8272b51499c38df2787a669116847d
robinmalkus <robinmalku@gmail.com>
2021-03-28 16:32:15 -0400

    adding scoring.js file

---
+++ scoring.js
@@ -0,0 +1,86 @@
+const calcuateScore = () => {
+  
+   // create returnTotal
+   
+   // if QB then do this
+
+   switch (calcuateScore() .tests.js()) {  
+    case 0:
+        Passing = "1";
+        break;
+    case 1: 
+        Touchdowns = "6"
+        break;
+    case 2:
+        Interceptions = "-3"
+        break;
+    case 3:
+       Yards = "1"
+        break;
+    case 4:
+        Rushing = "-3"
+        break;
+
+   // if RB then do this
+   case 5:
+       Passing = "1"
+       break;
+    case 6:
+        touchdowns = "6"
+        break;
+    case 7:
+        Fumble = "-3"
+        break;
+    case 8:
+        reception = "1"
+        break;
+      
+   //if TE then do this
+    case 9:
+        reception = "1"
+        break;
+    case 10:
+        Yards = "1"
+        break;
+    case 11:
+        Touchdowns = "6"
+        break;
+    case 12:
+        Fumble = "-3"
+        break;
+
+   //if WR then do this
+    case 13:
+        Rushing = "1"
+        break;
+    case 14:
+        Touchdowns = "6"
+        break;
+    case 15:
+        Fumble = "-3"
+        break;
+    case 16:
+        reception = "1"
+        break;
+    case 17:
+        Recieving = "1"
+        break;
+    case 18:
+        Return = "1"
+        break;
+    case 19:
+        Bunt = "6"
+        break;
+
+   // if no position return  0
+   case 20:
+       Postion = "0"
+       break;
+
+   //return result
+
+
+
+}
+
+module.exports = calculateScore
